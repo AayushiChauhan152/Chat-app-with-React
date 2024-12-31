9dd2c79ceca0f0ecdf3ffbacb911c8142fec1256
Aayushi Chauhan <96294707+AayushiChauhan152@users.noreply.github.com>
2024-12-31 21:30:28 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore, collection } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAkC3gfb7ogqAVvcbMfDXz3wH08W23Cqr0",
-  authDomain: "bytegalchatapp.firebaseapp.com",
-  projectId: "bytegalchatapp",
-  storageBucket: "bytegalchatapp.firebasestorage.app",
-  messagingSenderId: "965609146809",
-  appId: "1:965609146809:web:54f7690f9478be340cce68",
-  measurementId: "G-ZTZ23HB9JC",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: "",
 };
 
 export const app = initializeApp(firebaseConfig);
